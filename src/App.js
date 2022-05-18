f02531039fedf23695970d238cd37440ad0d0d6b
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-18 20:37:19 +0530

    bootstarp imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import Button from 'react-bootstrap/Button'
+import  Container from "react-bootstrap/Container";
+import Stack from 'react-bootstrap/Stack'
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div style= {{display:"gird", }}></div>
+    </Container>
+  );
 }
 
 export default App;
